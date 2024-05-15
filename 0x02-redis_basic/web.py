1c9ad917aad8af1d6f7657cede85cf5fcd6860de
Nayra-Adel19 <nayra.adel19@gmail.com>
2024-05-15 13:43:56 -0700

    0x02. Redis basic 2 :D

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,26 +1,23 @@
 #!/usr/bin/env python3
-"""Inside get_page track how many times particular URL"""
+""" Web Cache & Redis """
 import redis
 import requests
 from functools import wraps
 from typing import Callable
 
-
 redis_store = redis.Redis()
-"""Inside get_page track how many times particular URL"""
 
 
 def data_cacher(method: Callable) -> Callable:
-    """Inside get_page track how many times particular URL"""
+    """ Data Cacher """
     @wraps(method)
     def invoker(url) -> str:
-        """Inside get_page track how many times particular URL"""
+        """ Invoker """
         redis_store.incr(f'count:{url}')
         result = redis_store.get(f'result:{url}')
         if result:
             return result.decode('utf-8')
         result = method(url)
-        redis_store.set(f'count:{url}', 0)
         redis_store.setex(f'result:{url}', 10, result)
         return result
     return invoker
@@ -28,5 +25,5 @@ def data_cacher(method: Callable) -> Callable:
 
 @data_cacher
 def get_page(url: str) -> str:
-    """Inside get_page track how many times particular URL"""
+    """ Get Page """
     return requests.get(url).text
